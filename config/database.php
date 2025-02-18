32aa31bf9d40e95b92f8b714cc397504c854a3f4
Natihermida <hermidaltamarnati@gmail.com>
2025-02-18 20:17:09 +0100

    mvc cambios

--- config/database.php
+++ config/database.php
@@ -1,13 +1,21 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "apptask";
-try {
-    $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
+require "vendor/autoload.php";
+require"config.php";//  importar la clase autoload de composer
+use Illuminate\Database\Capsule\Manager as Capsule;//importar la clase Capsule de Eloquent
+
+$capsule = new Capsule;
+
+$capsule->addConnection([//configuracion de la base de datos
+    'driver' => 'mysql',
+    'host' => HOST,
+    'database' => DB_DATABASE,
+    'username' => DB_USER,
+    'password' => DB_PASS,
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-require_once './config/database.php';//requiero el archivo de configuracion de la base de datos
-use Formacom\core\App;//uso el namespace de la clase App
+require_once("./config/database.php");
+use Formacom\Core\App;
+//use Formacom\core\App;//uso el namespace de la clase App
 $app=new App();//creo un objeto de la clase App
 
 
