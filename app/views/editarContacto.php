36860e79158078ca07f16bf72f1bb80fb7cc4799
Natihermida <hermidaltamarnati@gmail.com>
2025-02-20 19:15:43 +0100

    cambios

--- app/views/editarContacto.php
+++ app/views/editarContacto.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="<?= BASE_URL(); ?>assets/css/style.css">
+   <link rel="stylesheet" href="<?= BASE_URL?>css/style.css">
     <title>Editar Contacto</title>
 
 </head>
--- app/views/listaContactos.php
+++ app/views/listaContactos.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASE_URL(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
     <title>Lista de Contactos</title>
 </head>
+
 <body>
     <h1>Lista de Contactos</h1>
-    
+
     <!-- Mostrar las tareas -->
     <?php
-    foreach($data as $tarea):?>
+    foreach ($data as $tarea): ?>
+
         <li>
-            <strong><?php echo $tarea->nombre;?></strong>:
-            <?php $tarea->telefono;?>
-            <a href="/agenda/agenda/edit/<?=$tarea->id_contactos?>">Editar</a>
-            <a href="/agenda/agenda/delete/<?=$tarea->id_contactos?>">Eliminar</a>
+            <strong><?php echo $tarea->nombre; ?></strong>:
+            <?php $tarea->telefono; ?>
+            <a href="/agenda/agenda/edit/<?= $tarea->id_contactos ?>">Editar</a>
+            <a href="/agenda/agenda/delete/<?= $tarea->id_contactos ?>">Eliminar</a>
         </li>
-        <?php endforeach; ?>
-    
-    
+    <?php endforeach; ?>
+
+
 
     <br>
-    <a href="/agenda/contacto/new/">Agregar nuevo contacto</a>
+         <a href="/agenda/contacto/new/">Agregar nuevo contacto</a>
 </body>
+
 </html>
